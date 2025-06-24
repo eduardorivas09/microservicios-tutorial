eac57de5b4fe66ac6e364ef353410983539ac267
Nestor Eduardo Rivas Oviedo <nerivas@inss.gob.ni>
2025-06-24 16:04:14 -0600

    Actualiza FeignClient del microservicio usuario-service

--- usuario-service/src/main/java/com/usuario/service/feignClients/CarroFeignClient.java
+++ usuario-service/src/main/java/com/usuario/service/feignClients/CarroFeignClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(name = "carro-service", url = "http://localhost:8002/carro")
+@FeignClient(name = "carro-service")
 public interface CarroFeignClient {
 
     @PostMapping()
